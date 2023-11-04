542068c5e87302dc0a65cc165ec99d2c7fc99325
Tomasz Skrzyp <stomasz296@gmail.com>
2023-11-04 01:19:32 +0100

    hm

--- C++/Projekt/doxy/html/search/functions_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['main_0',['main',['../check_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;check.cpp'],['../_clust_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;Clust.cpp'],['../data__create_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main():&#160;data_create.cpp']]]
-];
--- C++/Projekt/doxy/html/search/variables_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['global_0',['global',['../_clust_8cpp.html#ae009728a93e756481af1db3c402acb08',1,'Clust.cpp']]]
-];
