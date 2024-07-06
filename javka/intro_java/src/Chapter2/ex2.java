1cccea1b6febd48c1fadd074e342bad7d46ebf9f
Tomasz Skrzyp <stomasz296@gmail.com>
2024-07-06 22:28:50 +0200

    java exp

---
+++ javka/intro_java/src/Chapter2/ex2.java
@@ -0,0 +1,8 @@
+package Chapter2;
+
+public class ex2 {
+    public static void main(String[] args){
+        int dieroll=(int)(Math.random()*6);
+        System.out.println("First die came to "+dieroll);
+    }
+}
