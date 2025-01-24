16d2f0359d270a782df9958d521ef7cde96a965a
Tomasz Skrzyp <stomasz296@gmail.com>
2025-01-24 15:02:44 +0100

    ff

--- cheapdrive_main/cheapdrive_main/cheapdrive_app/cheapdrive_logic/start/distance_manager.py
+++
@@ -1,8 +0,0 @@
-from user_input import load_other_data
-
-def need_refill(distance,current_fuel,fuel_usage):
-    if (distance*fuel_usage/100)<(current_fuel):
-        return False
-    else:
-        return True
-        
